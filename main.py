8b69ece4efe6352685a7905821fca0fd4656e934
arzhangv <avaladkhani14@gmail.com>
2023-01-17 13:01:19 -0800

    added Submission requests

--- main.py
+++ main.py
@@ -28,6 +28,17 @@ class Reddit_Data:
         epoch_time = rawtime.timestamp()
         return int(epoch_time)
 
+    def writeToFileSubmission(self ,subreddit_name, date, data):
+        date_adjusted = date.replace(" ", "")
+        date_adjusted = date_adjusted.replace("-", "_")
+        date_adjusted = date_adjusted.replace(":", "_")
+
+        fileName = subreddit_name + "Submission" + date_adjusted + ".json"
+        filePath = "C:\\Users\\Arzhang\\PycharmProjects\\getRedditData\\malwareSubmission"
+        file = filePath + "\\" + fileName
+
+        with open(file, "w") as outfile:
+            json.dump(data, outfile)
     def writeToFile(self, subreddit_name, date, data):
         date_adjusted = date.replace(" ", "")
         date_adjusted = date_adjusted.replace("-", "_")
@@ -49,11 +60,14 @@ class Reddit_Data:
         epoch_after = str(self.get_epoch_time(after))
         response = requests.get("https://api.pushshift.io/reddit/search/comment/?after="+ epoch_before +"&before=" + epoch_after +"&subreddit=malware" )
 
+        submission_response = requests.get("https://api.pushshift.io/reddit/search/submission/?after="+ epoch_before +"&before=" + epoch_after +"&subreddit=malware")
         try:
             print(before)
             print(response.text)
             response_dict = json.loads(response.text)
+            submission_response_dict = json.loads(submission_response.text)
             self.writeToFile( "malware", str(before), response_dict)
+            self.writeToFileSubmission("malware",str(before), submission_response_dict)
 
         except json.decoder.JSONDecodeError:
             response_dict = None
@@ -61,7 +75,7 @@ class Reddit_Data:
 
     def master_loop(self):
         i = 0
-        for timestamp in self.datespan(datetime(2021, 1, 1, 0,1), datetime(2023, 1, 3, 0, 1),delta=timedelta(days=1)):
+        for timestamp in self.datespan(datetime(2022, 6, 20, 0,1), datetime(2023, 1, 3, 0, 1),delta=timedelta(days=1)):
             if i == 0:
                 before = timestamp
             elif i == 1:
